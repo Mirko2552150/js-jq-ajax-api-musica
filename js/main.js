c81d5b0e1bc95249852ae2399aab7fac9fb0ee8b
Mirko <mirko.longo.1987@gmail.com>
2020-03-17 14:58:18 +0100

    Creazione file

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// chiamata con AJAX (JQUERY) all'API boolean
+$.ajax({
+    url: "https://flynn.boolean.careers/exercises/api/array/music",
+    method: "GET",
+    success: function() {
+        
+    },
+    error: function() {
+
+    }
+});
